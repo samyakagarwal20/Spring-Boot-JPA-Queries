61befd9108b51c8bffa27767d7e5036db6ed54de
samyak <samyakagarwal20@gmail.com>
2023-08-02 13:58:51 +0530

    added changes for named query - approach 1

--- src/main/java/com/yflash/tech/SpringJPAQueries/repository/UserRepository.java
+++ src/main/java/com/yflash/tech/SpringJPAQueries/repository/UserRepository.java
@@ -2,8 +2,15 @@ package com.yflash.tech.SpringJPAQueries.repository;
 
 import com.yflash.tech.SpringJPAQueries.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
+
+    // Using entity name and fields for query rather than the actual table name and its columns for NamedQuery
+    @Query(value = "SELECT u FROM UserEntity u WHERE u.Id = :id")
+    UserEntity findUser(@Param("id") Integer id);
+
 }
