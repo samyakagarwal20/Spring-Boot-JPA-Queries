bc76a22616f214854051e9e88daf062afccfc891
samyak <samyakagarwal20@gmail.com>
2023-08-02 13:49:52 +0530

    added changes for native query

--- src/main/java/com/yflash/tech/SpringJPAQueries/repository/UserRepository.java
+++ src/main/java/com/yflash/tech/SpringJPAQueries/repository/UserRepository.java
@@ -2,8 +2,14 @@ package com.yflash.tech.SpringJPAQueries.repository;
 
 import com.yflash.tech.SpringJPAQueries.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
+
+    @Query(value = "SELECT * FROM USER u WHERE u.Id = :id", nativeQuery = true)
+    UserEntity findUser(@Param("id") Integer id);
+
 }
