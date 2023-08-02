8aafea40450d132ed83fc88c0d962ee618763068
samyak <samyakagarwal20@gmail.com>
2023-08-03 02:42:50 +0530

    added changes for derived query

--- src/main/java/com/yflash/tech/SpringJPAQueries/service/impl/UserServiceImpl.java
+++ src/main/java/com/yflash/tech/SpringJPAQueries/service/impl/UserServiceImpl.java
@@ -11,6 +11,8 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
 
